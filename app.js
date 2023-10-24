6d8b3b583def66c0e00d7387f54f36123eedc594
Blake Julander <blakejulander@gmail.com>
2023-10-24 07:39:46 -0700

    Started a simple guessing game to help me understand while loops

---
+++ app.js
@@ -0,0 +1,8 @@
+let maximum = parseInt(prompt("Enter a max number"))
+
+while (!maximum) {
+    maximum = parseInt(prompt("Enter a valid number"))
+}
+
+const targetNum = Math.floor(Math.random() * maximum) + 1
+console.log(targetNum)
