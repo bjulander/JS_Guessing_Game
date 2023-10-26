e7beb4a7ef4f332faeeddbe98cdeec61c524a25f
Blake Julander <blakejulander@gmail.com>
2023-10-26 07:08:14 -0700

    Finished my very simple guessing game

--- app.js
+++ app.js
@@ -6,3 +6,17 @@ while (!maximum) {
 
 const targetNum = Math.floor(Math.random() * maximum) + 1
 console.log(targetNum)
+
+let guess = parseInt(prompt("Enter your first guess!"))
+let attempts = 1
+
+while (parseInt(guess) !== targetNum) {
+    if (guess === 'q' || guess === 'quit') break;
+    attempts++;
+    if (guess > targetNum) {
+        guess = prompt("Too High! Try Again...")
+    } else {
+        guess = prompt("Too Low! Try Again...")
+    }
+}
+console.log(`You got it! It took you ${attempts} guesses bitch.`)
